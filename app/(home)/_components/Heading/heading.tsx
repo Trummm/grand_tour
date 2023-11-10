e02ffd7ce6bb81cd72025b17cdb7ad84209ecead
Trummm <trungpro2000.tp2@gmail.com>
2023-11-10 08:56:49 +0700

    another setup

---
+++ app/(home)/_components/Heading/heading.tsx
@@ -0,0 +1,7 @@
+import './heading.scss'
+
+const Heading = () => {
+  return <h1 className='Heading'>This is Header</h1>
+}
+
+export default Heading
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,9 @@
+import Heading from './_components/Heading/heading'
+
 export default function Home() {
   return (
     <>
+      <Heading />
       <h1>This is Home Page</h1>
     </>
   )
